59dd8d3d6834cad0e6b21b2bc3ae1951506b5948
luke141 <ekdns9799@naver.com>
2023-05-30 12:45:44 +0900

    자바: 프로젝트 생성 및 자바 샘플 코드 추가

---
+++ java/src/day1/sample/JavaSample.java
@@ -0,0 +1,19 @@
+package day1.sample;
+
+public class JavaSample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 한줄 주석 : 코드에 영향이 없는 설명 문구
+		/* 여러줄 주석
+		 */
+		/**
+		 * 메소드를 설명하는 주석 : 메소드를 배울 때 다시 설명
+		 * @param args
+		 */
+		// main : 프로그램이 실행되기 우해 필요한 것(메소드)
+		System.out.pritln("Hello Java");
+		//콘솔창에 원하는 문구를 출력하는 기능을 가진 것(메소드)
+	}
+
+}
