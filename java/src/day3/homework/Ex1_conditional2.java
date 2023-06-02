f052b41e58d0866104d433ccb476733efb199757
luke141 <ekdns9799@naver.com>
2023-06-02 14:34:39 +0900

    자바 : 3일차 과제 풀이

--- java/src/day3/homework/Ex1_conditional2.java
+++ java/src/day3/homework/Ex1_conditional2.java
@@ -2,7 +2,7 @@ package day3.homework;
 
 import java.util.Scanner;
 
-public class Ex1_conditional {
+public class Ex1_conditional2 {
 
 	public static void main(String[] args) {
 		/*성별(M:남성,W:여성)를 입력받아 여성인지 확인하는 코드를 작성하세요. 조건 연산자 이용
@@ -15,11 +15,11 @@ public class Ex1_conditional {
 		W
 		Are you a woman? true
 		* */
-		char gender; //필요한 변수 설정, ''=>빈문자는 불가능 but ""=>빈 문자열은 가능
+		String gender; //필요한 변수 설정, ''=>빈문자는 불가능 but ""=>빈 문자열은 가능 char=>문자, String=>문자열
 		Scanner sc = new Scanner(System.in);
 		System.out.println("input gender : ");
-		gender = sc.next().charAt(0);
-		boolean result = gender == 'w' ? false : true;
+		gender = sc.next();
+		boolean result = gender.equals("W") ? true : false;
 		System.out.println("Are you a women? " + result);
 		sc.close();//꼭 필요한건 아님
 		
--- java/src/day3/homework/Ex2_if.java
+++ java/src/day3/homework/Ex2_if.java
@@ -1,19 +1,30 @@
 package day3.homework;
 
+import java.util.Scanner;
+
 public class Ex2_if {
 
 	public static void main(String[] args) {
-		/*성별(M:남성,W:여성)를 입력받아 여성인지 확인하는 코드를 작성하세요. 조건 연산자 이용
-(package : day3.homework, file : Ex1_conditional) 
-input gender : 
-M
-Are you a women? false
-
-input gender : 
-W
-Are you a women? true
-
-		 * */
+		/*성별(M:남성,W:여성)를 입력받아 M이면 남성을, W이면 여성을 출력하는 코드를 작성하세요. if문 이용
+		(package : day3.homework, file : Ex1_ifjava) 
+		input gender : 
+		M
+		Are you a woman? man
+		input gender : 
+		W
+		Are you a woman? women
+		* */
+		char gender;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("input gender : ");
+		gender = sc.next().charAt(0);
+		if(gender == 'M') {
+			System.out.println("Are you a woman?" + "man");
+		}
+		if(gender == 'w') {
+			System.out.println("Are you a woman?" + "woman");
+		}
+		sc.close();
 	}
 
 }
