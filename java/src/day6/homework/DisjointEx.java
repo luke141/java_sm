1573ff974334e5965f42fb4f9ac239726dc29927
luke141 <ekdns9799@naver.com>
2023-06-08 09:15:12 +0900

    자바 : 6일차 과제(서로소 예제)

--- java/src/day6/homework/DisjointEx.java
+++ java/src/day6/homework/DisjointEx.java
@@ -1,5 +1,7 @@
 package day6.homework;
 
+import java.util.Scanner;
+
 public class DisjointEx {
 
 	public static void main(String[] args) {
@@ -10,7 +12,40 @@ public class DisjointEx {
 		//Disjoint!(서로소)
 		//input 2 numbers : 8, 4
 		//No disjoint!(서로소아님)
-
+		/*/반복횟수 : i는 1부터 num1까지 1씩 증가
+		* 규칙성 : i가 num1의 약수이고 i가 num2의 약수이면 i를 gcd에 저장 
+		* 		  num1을 i로 나누었을 때 나머지가 0과 같고
+		* 		  num2를 i로 나누었을 때 나머지가 0과 같다면
+		* 		  i를 gcd에 저장
+		*
+		* 반복문 종료 후 : gcd가 1이면 Disjoint를 출력하고, 아니면 Not joint를 출력
+		*/
+		int num1, num2, i = 1, gcd = 1;
+		Scanner sc = new Scanner(System.in);
+		//두 정수를 입력
+		System.out.println("input 2 numbers : ");
+		num1 = sc.nextInt();
+		num2 = sc.nextInt();
+		
+		//반복문(최대공약수를 구하기 위한) : i가 num1까지
+		for(i=1, gcd = 1 ;i  <= num1; i++) {
+			//num1을 i로 나누었을 때 나머지가 0과 같고 num2를 i로 나누었을 때 나머지가 0과 같다면 i를 gcd에 저장
+			if(num1 % i == 0 && num2 % i == 0) {
+				gcd = i;
+			}
+			//i를 1증가
+			i++;
+		
+	//gcd를 출력
+	if(gcd == 1) {   
+		System.out.println("Disjoint!");
+	}else {
+		System.out.println("Not disjoint!");
 	}
-
+	sc.close();
+		}
+	}
 }
+	
+
+
