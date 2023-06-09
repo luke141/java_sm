05c151b2831d452aff712e6a8746e4bba071396c
luke141 <ekdns9799@naver.com>
2023-06-09 14:41:45 +0900

    자바 : 8일차 homework

---
+++ java/src/day8/homework/MethodPrintCharEx.java
@@ -0,0 +1,18 @@
+package day8.homework;
+
+public class MethodPrintCharEx {
+
+	public static void main(String[] args) {
+		/* 다음과 같이 출력하기 위한 코드를 작성하세요.
+		 * 단 메서드를 이용
+		 * */
+		//*****
+		//***
+		//?????
+		//^^^^^^^
+		
+		
+
+	}
+
+}
